134d7eb2741ad8dce1f8696bf26d2d52b3ba0cf9
Ujaval Gandhi <ujaval@spatialthoughts.com>
2020-06-25 22:41:32 +0530

    package script

---
+++ python-foundation-package.sh
@@ -0,0 +1,12 @@
+OUTPUT_DIR=~/Downloads/
+PACKAGE_DIR=~/Downloads/python_foundation
+SOLUTIONS_DIR=~/Downloads/python_foundation_solutions
+cp -R code/python_foundation/data $PACKAGE_DIR/
+rm -R $PACKAGE_DIR/data/.ipynb_checkpoints $PACKAGE_DIR/data/*/.ipynb_checkpoints
+cp code/python_foundation/[0-9]*.ipynb $PACKAGE_DIR
+rm $PACKAGE_DIR/10_*.ipynb
+#jupyter-nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=120 $PACKAGE_DIR/*.ipynb
+#rm -R $PACKAGE_DIR/output
+cp code/python_foundation/[0-9]*.ipynb $PACKAGE_DIR
+jupyter-nbconvert --ClearOutputPreprocessor.enabled=True --inplace $PACKAGE_DIR/*.ipynb
+cp code/python_foundation/solutions/*.ipynb $SOLUTIONS_DIR
