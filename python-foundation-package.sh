1540389b3f2da21ad82c94e1ae02ef13743a0f3e
Ujaval Gandhi <ujaval@spatialthoughts.com>
2020-06-25 22:52:23 +0530

    fix script

--- python-foundation-package.sh
+++ python-foundation-package.sh
@@ -2,6 +2,8 @@ OUTPUT_DIR=~/Downloads/
 PACKAGE_DIR=~/Downloads/python_foundation
 SOLUTIONS_DIR=~/Downloads/python_foundation_solutions
 cp -R code/python_foundation/data $PACKAGE_DIR/
+mkdir -p $PACKAGE_DIR/images/python_foundation
+cp -R images/python_foundation/package/* $PACKAGE_DIR/images/python_foundation/
 rm -R $PACKAGE_DIR/data/.ipynb_checkpoints $PACKAGE_DIR/data/*/.ipynb_checkpoints
 cp code/python_foundation/[0-9]*.ipynb $PACKAGE_DIR
 rm $PACKAGE_DIR/10_*.ipynb
