04c841fb16f10b8c8542f7b240d1ee8377b28a20
Ujaval Gandhi <ujaval@spatialthoughts.com>
2020-09-08 00:41:58 +0530

    update site

--- docs/python-foundation-package.sh
+++ docs/python-foundation-package.sh
@@ -1,15 +1,14 @@
 OUTPUT_DIR=~/Downloads/
 PACKAGE_DIR=~/Downloads/python_foundation
 SOLUTIONS_DIR=~/Downloads/python_foundation_solutions
-cp -R code/python_foundation/data $PACKAGE_DIR/data
 mkdir -p $PACKAGE_DIR/images/python_foundation
 mkdir -p $PACKAGE_DIR/data
+mkdir -p $PACKAGE_DIR/output
 mkdir -p $SOLUTIONS_DIR
 cp -R images/python_foundation/* $PACKAGE_DIR/images/python_foundation/
+cp -R code/python_foundation/data/* $PACKAGE_DIR/data
 rm -R $PACKAGE_DIR/data/.ipynb_checkpoints $PACKAGE_DIR/data/*/.ipynb_checkpoints
-#rm $PACKAGE_DIR/10_*.ipynb
-#jupyter-nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=120 $PACKAGE_DIR/*.ipynb
-#rm -R $PACKAGE_DIR/output
+# Clear output from cells before packaging
 jupyter-nbconvert --ClearOutputPreprocessor.enabled=True --inplace code/python_foundation/[0-9]*.ipynb
 jupyter-nbconvert --to markdown  code/python_foundation/[0-9]*.ipynb --output-dir .
 cp code/python_foundation/[0-9]*.ipynb $PACKAGE_DIR/
