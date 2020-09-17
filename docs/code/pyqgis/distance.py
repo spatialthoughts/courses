8ee407c563586d5cecc837098d70709e05281e4c
Ujaval Gandhi <ujaval@spatialthoughts.com>
2020-09-18 00:23:03 +0530

    add distance exercise

--- docs/code/pyqgis/distance.py
+++ docs/code/pyqgis/distance.py
@@ -1,36 +1,17 @@
-import math
+from qgis.core import QgsDistanceArea
 
-def ellipsoid_distance(origin, destination):
-  lat1, lon1 = origin
-  lat2, lon2 = destination
-  # Remember the order is X,Y
-  point1 = QgsPointXY(lon1, lat1)
-  point2 = QgsPointXY(lon2, lat2)
+san_francisco = (37.7749, -122.4194)
+new_york = (40.661, -73.944)
 
-  d = QgsDistanceArea()
-  d.setEllipsoid('WGS84')
+d = QgsDistanceArea()
+d.setEllipsoid('WGS84')
 
-  #Measure the distance
-  distance = d.measureLine([point1, point2])
-  return distance/1000
-  
-def haversine_distance(origin, destination):
-  lat1, lon1 = origin
-  lat2, lon2 = destination
-  radius = 6371 # km
-  dlat = math.radians(lat2-lat1)
-  dlon = math.radians(lon2-lon1)
-  a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-    * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-  c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-  distance = radius * c
-  return distance
-  
-origin = 12.98, 77.58 # Bangalore
-destination = 12.30, 76.64 # Mysore
 
-d1 = haversine_distance(origin, destination)
-d2 = ellipsoid_distance(origin, destination)
+lat1, lon1 = san_francisco
+lat2, lon2 = new_york
+# Remember the order is X,Y
+point1 = QgsPointXY(lon1, lat1)
+point2 = QgsPointXY(lon2, lat2)
 
-print('Haversine distance: {} km'.format(d1))
-print('Ellipsoid distance: {} km'.format(d2))
+distance = d.measureLine([point1, point2])
+print(distance/1000)
--- docs/code/pyqgis/solutions/distance.py
+++ docs/code/pyqgis/solutions/distance.py
@@ -2,16 +2,22 @@ from qgis.core import QgsDistanceArea
 
 san_francisco = (37.7749, -122.4194)
 new_york = (40.661, -73.944)
+las_vegas = (36.1699, -115.1398)
 
 d = QgsDistanceArea()
 d.setEllipsoid('WGS84')
 
 
 lat1, lon1 = san_francisco
-lat2, lon2 = new_york
+lat2, lon2 = las_vegas
+lat3, lon3 = new_york
 # Remember the order is X,Y
 point1 = QgsPointXY(lon1, lat1)
 point2 = QgsPointXY(lon2, lat2)
+point3 = QgsPointXY(lon3, lat3)
 
-distance = d.measureLine([point1, point2])
+distance = d.measureLine([point1, point2, point3])
 print(distance/1000)
+
+distance_mi = d.convertLengthMeasurement(distance, QgsUnitTypes.DistanceMiles)
+print(distance_mi)
