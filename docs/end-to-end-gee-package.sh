6839ce8967631778c4e5d2f6ec64538efe93b993
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-03-28 21:25:44 +0530

    start intermediate module

--- docs/end-to-end-gee-package.sh
+++ docs/end-to-end-gee-package.sh
@@ -1,3 +1,4 @@
 rm -R code/end_to_end_gee
 mkdir code/end_to_end_gee
 cp -R ~/projects/End-to-End-GEE/01-Earth-Engine-Basics code/end_to_end_gee/
+cp -R ~/projects/End-to-End-GEE/02-Earth-Engine-Intermediate code/end_to_end_gee/
