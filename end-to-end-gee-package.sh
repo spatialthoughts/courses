87102654262c6fc24043f0a9edb4cf6bb34e90ed
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-03-29 18:16:57 +0530

    supervised classification

--- end-to-end-gee-package.sh
+++ end-to-end-gee-package.sh
@@ -2,3 +2,4 @@ rm -R code/end_to_end_gee
 mkdir code/end_to_end_gee
 cp -R ~/projects/End-to-End-GEE/01-Earth-Engine-Basics code/end_to_end_gee/
 cp -R ~/projects/End-to-End-GEE/02-Earth-Engine-Intermediate code/end_to_end_gee/
+cp -R ~/projects/End-to-End-GEE/03-Supervised-Classification code/end_to_end_gee/
