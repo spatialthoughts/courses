b9f55871d2ff9a8cf161bae50d835094d84c4827
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-04-30 18:57:00 +0530

    add advanced module materials

--- end-to-end-gee-package.sh
+++ end-to-end-gee-package.sh
@@ -1,5 +1,12 @@
-rm -R code/end_to_end_gee
-mkdir code/end_to_end_gee
+# rm -R code/end_to_end_gee
+# mkdir code/end_to_end_gee
 cp -R ~/projects/End-to-End-GEE/01-Earth-Engine-Basics code/end_to_end_gee/
 cp -R ~/projects/End-to-End-GEE/02-Earth-Engine-Intermediate code/end_to_end_gee/
 cp -R ~/projects/End-to-End-GEE/03-Supervised-Classification code/end_to_end_gee/
+cp -R ~/projects/End-to-End-GEE/04-Earth-Engine-Advanced code/end_to_end_gee/
+cp -R ~/projects/End-to-End-GEE/Supplement code/end_to_end_gee/
+# Python notebooks
+# Clear output from cells before packaging
+jupyter-nbconvert --ClearOutputPreprocessor.enabled=True --inplace code/end_to_end_gee/*.ipynb
+jupyter-nbconvert --to markdown  code/end_to_end_gee/*.ipynb --output-dir .
+
