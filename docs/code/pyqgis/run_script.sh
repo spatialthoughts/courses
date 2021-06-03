865f794aca920c9b9cad793f99b97f502cde3030
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-06-03 20:22:01 +0530

    update script

--- docs/code/pyqgis/run_script.sh
+++ docs/code/pyqgis/run_script.sh
@@ -1,9 +1,9 @@
-export PYQGIS_STARTUP=/Applications/QGIS3.10.app/Contents/Resources/python/pyqgis-startup.py
-export PYTHONHOME=/Applications/QGIS3.10.app/Contents/Frameworks/Python.framework/Versions/Current
-export PYTHONPATH=/Applications/QGIS3.10.app/Contents/Resources/python:${PYTHONPATH}
-export QGIS_PREFIX_PATH=/Applications/QGIS3.10.app/Contents/MacOS
-export GDAL_DRIVER_PATH=/Applications/QGIS3.10.app/Contents/Resources/gdal/gdalplugins
-export GDAL_DATA=/Applications/QGIS3.10.app/Contents/Resources/gdal
-export QT_QPA_PLATFORM_PLUGIN_PATH=/Applications/QGIS3.10.app/Contents/Plugins
+export PYQGIS_STARTUP=/Applications/QGIS-LTR.app/Contents/Resources/python/pyqgis-startup.py
+export PYTHONHOME=/Applications/QGIS-LTR.app/Contents/Frameworks/Python.framework/Versions/Current
+export PYTHONPATH=/Applications/QGIS-LTR.app/Contents/Resources/python:${PYTHONPATH}
+export QGIS_PREFIX_PATH=/Applications/QGIS-LTR.app/Contents/MacOS
+export GDAL_DRIVER_PATH=/Applications/QGIS-LTR.app/Contents/Resources/gdal/gdalplugins
+export GDAL_DATA=/Applications/QGIS-LTR.app/Contents/Resources/gdal
+export QT_QPA_PLATFORM_PLUGIN_PATH=/Applications/QGIS-LTR.app/Contents/Plugins
 
 python3 save_attributes_standalone.py
