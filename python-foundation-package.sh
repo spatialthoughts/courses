238f09d7790ef8ece5fc38223787672b15bc4ea5
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-09-17 18:51:02 +0530

    add python supplement

--- python-foundation-package.sh
+++ python-foundation-package.sh
@@ -9,7 +9,7 @@ cp -R images/python_foundation/* $PACKAGE_DIR/images/python_foundation/
 cp -R code/python_foundation/data/* $PACKAGE_DIR/data
 rm -R $PACKAGE_DIR/data/.ipynb_checkpoints $PACKAGE_DIR/data/*/.ipynb_checkpoints
 # Clear output from cells before packaging
-jupyter-nbconvert --ClearOutputPreprocessor.enabled=True --inplace code/python_foundation/[0-9]*.ipynb
-jupyter-nbconvert --to markdown  code/python_foundation/[0-9]*.ipynb --output-dir .
-cp code/python_foundation/[0-9]*.ipynb $PACKAGE_DIR/
+jupyter-nbconvert --ClearOutputPreprocessor.enabled=True --inplace code/python_foundation/*.ipynb
+jupyter-nbconvert --to markdown  code/python_foundation/*.ipynb --output-dir .
+cp code/python_foundation/*.ipynb $PACKAGE_DIR/
 cp code/python_foundation/solutions/*.ipynb $SOLUTIONS_DIR/
