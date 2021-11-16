e1875a6f4a413cdc0a7e1f47d3fb96c2083a3520
Ujaval Gandhi <ujaval@spatialthoughts.com>
2021-11-16 15:02:37 +0530

    add water resources course

---
+++ docs/gee-water-resources-management-package.sh
@@ -0,0 +1,4 @@
+# rm -R code/gee_water_resources_management
+# mkdir code/gee_water_resources_management
+cp -R ~/projects/GEE-Water-Resources-Management/01-Earth_Engine_Basics code/gee_water_resources_management/
+
