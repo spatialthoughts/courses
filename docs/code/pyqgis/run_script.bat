99e5e0f82464f40e9b95332aa9373920a6bab527
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-07-13 12:12:25 +0530

    add supplement

--- docs/code/pyqgis/run_script.bat
+++ docs/code/pyqgis/run_script.bat
@@ -1,11 +1,11 @@
 @echo off 
 
-set OSGEO4W_ROOT=C:\OSGeo4W64
+:: If you get an error, check if the folder below exists.
+set OSGEO4W_ROOT=C:\OSGeo4W
  
 call "%OSGEO4W_ROOT%\bin\o4w_env.bat" 
-call "%OSGEO4W_ROOT%\bin\qt5_env.bat" 
-call "%OSGEO4W_ROOT%\bin\py3_env.bat" 
- 
+
+:: If you are not using QGIS LTR version, change qgis-ltr to qgis in all lines below
 set PATH=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis-ltr\bin;C:\OSGeo4W64\apps\Qt5\bin;%PATH%
 set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-ltr\python;%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins;%PYTHONPATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%\apps\qgis-ltr
