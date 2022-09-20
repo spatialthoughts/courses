ce3895b85d6ce5506024efce3c2d1b995b6da184
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-09-20 16:05:20 +0530

    Update 10_leafmap_basics.py

--- code/python_dataviz/solutions/10_leafmap_basics.py
+++ code/python_dataviz/solutions/10_leafmap_basics.py
@@ -1,9 +1,4 @@
 m = leafmap.Map(width=800, height=500, google_map='SATELLITE')
-
-bounds = leafmap.cog_bounds(ntl2015_url)
-m.zoom_to_bounds(bounds)
-
-m.add_cog_layer(ntl2015_url, name='ntl2015')
-m.add_cog_layer(ntl2022_url, name='ntl2022')
-
+data_url = 'https://storage.googleapis.com/spatialthoughts-public-data/viirs_ntl_2021_india.tif'
+m.add_cog_layer(data_url, name='VIIRS NTL 2021', colormap_name='viridis')
 m
