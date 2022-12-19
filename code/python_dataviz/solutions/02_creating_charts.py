71e3767c98aba3e1ee79bdaf090630358780c200
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-12-19 13:33:09 +0530

    Update 02_creating_charts.py

--- code/python_dataviz/solutions/02_creating_charts.py
+++ code/python_dataviz/solutions/02_creating_charts.py
@@ -8,4 +8,12 @@ monthly_counts.plot(kind='line', ax=ax, color='red', marker='o')
 ax.set_title('Bicycle Thefts by Month')
 ax.set_ylabel('Total Incidents')
 
+# Extra: Add labels
+line = ax.lines[0]
+
+for x_value, y_value in zip(line.get_xdata(), line.get_ydata()):
+    ax.annotate(y_value,(x_value, y_value), xytext=(0, 20), 
+        textcoords="offset points", ha='center', va='top')
+plt.tight_layout()
+
 plt.show()
