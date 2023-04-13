829c66e7b72aadbcbf6d3caec4c33aefe67f8e41
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-04-13 18:38:42 +0530

    Update exercise4.py

--- code/qgis_actions/solutions/exercise4.py
+++ code/qgis_actions/solutions/exercise4.py
@@ -7,5 +7,12 @@ layer_name = os.path.splitext(file_name)[0]
 layer_list = QgsProject.instance().mapLayersByName(layer_name)
 
 if layer_list:
-    QgsProject.instance().removeMapLayer(layer_list[0])
-    #iface.mapCanvas().refresh()
+    iface.messageBar().pushCritical('Error', f'Raster Tile {file_name} already loaded.')
+else:
+    iface.addRasterLayer(path)
+    iface.messageBar().pushSuccess('Success', f'Raster Tile {file_name} loaded')
+
+layer_id = '[%@layer_id%]'
+layer = QgsProject.instance().mapLayer(layer_id)
+iface.setActiveLayer(layer)
+
