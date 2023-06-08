40a18dde770e6307fbfe47464ea52b57826ffda5
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-06-08 15:23:08 +0530

    add advanced charts

--- docs/gee-charts-package.sh
+++ docs/gee-charts-package.sh
@@ -1,2 +1,4 @@
 cp -R ~/projects/GEE-Charts/01-Time_Series_Charts code/gee_charts/
 cp -R ~/projects/GEE-Charts/02-Image_Charts code/gee_charts/
+cp -R ~/projects/GEE-Charts/03-FeatureCollection_Charts code/gee_charts/
+cp -R ~/projects/GEE-Charts/04-Advanced_Charts code/gee_charts/
