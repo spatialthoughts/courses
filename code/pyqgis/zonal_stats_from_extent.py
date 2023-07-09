ae9e5ef0fbbe4be5f46f2bbc5eec98ae633e1ea5
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-07-10 00:07:52 +0530

    new script

---
+++ code/pyqgis/zonal_stats_from_extent.py
@@ -0,0 +1,33 @@
+mc = iface.mapCanvas()
+extent = mc.extent()
+geometry = QgsGeometry.fromRect(extent)
+
+vlayer = QgsVectorLayer('Polygon?crs=EPSG:4326', 'extent', 'memory')
+provider = vlayer.dataProvider()
+
+f = QgsFeature()
+f.setGeometry(geometry)
+provider.addFeature(f)
+vlayer.updateExtents() 
+
+srtm = QgsProject.instance().mapLayersByName('srtm')[0]
+
+results = processing.run("native:zonalstatisticsfb", {
+  'INPUT': vlayer,
+  'INPUT_RASTER': srtm,
+  'RASTER_BAND':1,
+  'COLUMN_PREFIX':'elevation_',
+  'STATISTICS':[2],
+  'OUTPUT':'TEMPORARY_OUTPUT'})
+
+stats_layer = results['OUTPUT']
+
+features = stats_layer.getFeatures()
+feature = next(features)
+elevation = feature.attributes()[0]
+if elevation:
+  iface.messageBar().pushInfo(
+    'Processing done', f'Average Elevation in Extent {elevation:.2f}m')
+else:
+  iface.messageBar().pushCritical(
+    'Error', 'No pixels in current extent')
