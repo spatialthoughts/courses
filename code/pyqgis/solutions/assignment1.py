9d697c3f8493419d2ce645366193073abc738ef4
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-07-13 17:19:45 +0530

    Create assignment1.py

---
+++ code/pyqgis/solutions/assignment1.py
@@ -0,0 +1,38 @@
+import os
+import math
+
+qgis_dir = QgsApplication.prefixPath()
+icon_path = os.path.join(qgis_dir, 'svg', 'crosses', 'Cross2.svg')
+
+def show_statistics():
+    layer = iface.activeLayer()
+    # Check if a layer is selected
+    if not layer:
+        iface.messageBar().pushMessage('Please select a layer',  level=Qgis.Critical) 
+    # Check if the selected layer is a raster layer
+    if layer.type() != QgsMapLayer.RasterLayer:
+        iface.messageBar().pushMessage('Please select a raster layer',  level=Qgis.Critical)
+      
+    # Get the canvas extent
+    mc = iface.mapCanvas()
+    extent = mc.extent()
+    provider = layer.dataProvider()
+
+    # Get the raster statistics of band 1
+    stats = provider.bandStatistics(1, QgsRasterBandStats.All, extent)
+
+    # Get the average value
+    mean = stats.mean
+
+    # The value is NaN if there are no valid pixels in the extent
+    if not math.isnan(mean):
+        iface.messageBar().pushInfo(
+            'Processing done', f'Average Value in Extent {mean:.2f}')
+    else:
+        iface.messageBar().pushCritical(
+        'Error', 'No pixels in current extent')
+      
+action = QAction('Show Raster Statistics')
+action.triggered.connect(show_statistics)
+action.setIcon(QIcon(icon_path))
+iface.addToolBarIcon(action)
