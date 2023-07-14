4f0f5a9761a8c23c90978e3f25c6e98a3f70aec0
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-07-14 09:58:18 +0530

    update

--- docs/code/pyqgis/solutions/exercise5.py
+++ docs/code/pyqgis/solutions/exercise5.py
@@ -1,19 +1,25 @@
 import os
 data_dir = os.path.join(os.path.expanduser('~'), 'Downloads', 'pyqgis_masterclass')
 
-filename = 'srtm.tif'
-srtm = os.path.join(data_dir, filename)
-filename = 'shoreline.shp'
-shoreline = os.path.join(data_dir, filename) 
+vector_layer = 'seismic_zones.shp'
+vector_layer_path = os.path.join(data_dir, vector_layer)
 
-results = processing.run("gdal:cliprasterbymasklayer", 
-    {'INPUT':srtm,
-    'MASK': shoreline,
-    'OUTPUT':'TEMPORARY_OUTPUT'})
+raster_layer = 'srtm.tif'
+raster_layer_path = os.path.join(data_dir, raster_layer)
 
-clipped_dem = results['OUTPUT']
+# Input vector has invalid geometries
+# Fix them first
+results = processing.run("native:fixgeometries", {
+  'INPUT':vector_layer_path,
+  'METHOD': 0,
+  'OUTPUT':'TEMPORARY_OUTPUT'})
 
+fixed_vector_layer = results['OUTPUT']
 
-results = processing.run("qgis:rasterlayerstatistics", 
-    {'INPUT': clipped_dem, 'BAND': 1})
-print('Highest Elevation in San Francisco :{} m'.format(results['MAX']))
+processing.runAndLoadResults("native:zonalstatisticsfb", {
+  'INPUT': fixed_vector_layer,
+  'INPUT_RASTER': raster_layer_path,
+  'RASTER_BAND':1,
+  'COLUMN_PREFIX':'elevation_',
+  'STATISTICS':[2],
+  'OUTPUT':'TEMPORARY_OUTPUT'})
