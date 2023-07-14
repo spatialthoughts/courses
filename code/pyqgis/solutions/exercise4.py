b0b901d5159da6b9d790717f6f84008b6ef0acd9
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-07-14 13:09:54 +0530

    update

--- code/pyqgis/solutions/exercise4.py
+++ code/pyqgis/solutions/exercise4.py
@@ -1,6 +1,6 @@
 crsToolbar = iface.addToolBar('CRS Toolbar')
 
-label = QLabel('Enter a EPSG Code', parent=projectToolbar)
+label = QLabel('Enter an EPSG Code', parent=projectToolbar)
 crsTextBox = QLineEdit('4326', parent=projectToolbar)
 crsTextBox.setFixedWidth(80)
 button = QPushButton('Go!', parent=projectToolbar)
@@ -11,8 +11,18 @@ crsToolbar.addWidget(button)
 
 
 def changeCrs(crsText):
-    crsText = int(crsTextBox.text())
-    crs = QgsCoordinateReferenceSystem(crsText)
-    QgsProject.instance().setCrs(crs)
+    currentCrs = QgsProject.instance().crs()
+    currentCrsCode = currentCrs.authid()
+    epsgCode = int(crsTextBox.text())
+    crs = QgsCoordinateReferenceSystem(epsgCode)
+    newCrsCode = f'EPSG:{epsgCode}'
+    if newCrsCode == currentCrsCode:
+        iface.messageBar().pushInfo(
+        'No Change Applied', f'Project already set to {newCrsCode}')
+    else:
+        QgsProject.instance().setCrs(crs)
+        iface.messageBar().pushInfo(
+        'Project CRS Changed', newCrsCode)
+
 
 button.clicked.connect(changeCrs)
