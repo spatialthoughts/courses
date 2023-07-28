ab5bb00b68d6756ab51450df9eb58415063f489d
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-07-28 13:59:03 +0530

    add exercise

--- docs/code/pyqgis/solutions/exercise10.py
+++ docs/code/pyqgis/solutions/exercise10.py
@@ -1,35 +1,13 @@
+from qgis.utils import iface
+
+feature_name = '[%NAME%]'
+feature_id = [%$id%]
 layer_id = '[%@layer_id%]'
-fid = [% $id %]
 
 layer = QgsProject.instance().mapLayer(layer_id)
-
-def get_neighbors(fid):
-    f = layer.getFeature(fid)
-    # Use list comprehension to get all intersecting features
-    # You may also use touches() if your data is topologically correct
-    # Supply the bounding box to getFeatures() to use Spatial Index
-    neighbors = [
-        c.id()
-        for c in layer.getFeatures(f.geometry().boundingBox())
-        if c.geometry().intersects(f.geometry()) and c.id() != f.id()
-    ]
-    return neighbors
-
-
-first_degree_neighbors = get_neighbors(fid)
-
-second_degree_neighbors = set()
-
-for n in first_degree_neighbors:
-    neighbors = get_neighbors(n)
-    second_degree_neighbors.update(neighbors)
-
-# Remove all first-degree neighbors from the set
-second_degree_neighbors = second_degree_neighbors.difference(
-    set(first_degree_neighbors))
-    
-# Remove the feature itself from the set if it exists
-second_degree_neighbors.discard(fid)
-
-# Apply the selection
-layer.selectByIds(list(second_degree_neighbors), QgsVectorLayer.AddToSelection)
+new_layer = layer.materialize(
+    QgsFeatureRequest().setFilterFids([feature_id]))
+new_layer.setName(feature_name)
+QgsProject.instance().addMapLayer(new_layer)
+iface.setActiveLayer(layer)
+iface.messageBar().pushInfo('New Layer Created', feature_name)
