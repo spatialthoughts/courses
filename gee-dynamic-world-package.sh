c5a82add9f7a05a9e321a41ffbfb055a0ba55fea
Ujaval Gandhi <ujaval@spatialthoughts.com>
2023-11-23 13:43:17 +0530

    add dw workshop

---
+++ gee-dynamic-world-package.sh
@@ -0,0 +1,5 @@
+cp -R ~/projects/GEE-Dynamic-World/01_Change_Detection code/gee_dynamic_world/
+cp -R ~/projects/GEE-Dynamic-World/02_Supervised_Classification code/gee_dynamic_world/
+cp -R ~/projects/GEE-Dynamic-World/03_Time_Series_Processing code/gee_dynamic_world/
+cp -R ~/projects/GEE-Dynamic-World/04_Earth_Engine_Apps code/gee_dynamic_world/
+cp -R ~/projects/GEE-Dynamic-World/Supplement code/gee_dynamic_world/
