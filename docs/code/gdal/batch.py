c505cf0202654585f91b74dad433e5cd547ca035
Ujaval Gandhi <ujaval@spatialthoughts.com>
2024-02-29 16:47:31 +0530

    fix problems

--- docs/code/gdal/batch.py
+++ docs/code/gdal/batch.py
@@ -2,11 +2,12 @@ import os
 
 input_dir = 'naip'
 
-command = 'gdal_translate -of GTiff -co COMPRESS=JPEG {input} {output}'
+command_string = 'gdal_translate -of GTiff -co COMPRESS=JPEG {input} {output}'
 for file in os.listdir(input_dir):
   if file.endswith('.jp2'):
     input = os.path.join(input_dir, file)
     filename = os.path.splitext(os.path.basename(file))[0]
     output =  os.path.join(input_dir, filename + '.tif')
-    os.system(command.format(input=input, output=output))
-
+    command = command_string.format(input=input, output=output)
+    print('Running ->', command)
+    os.system(command)
--- docs/code/gdal/batch_parallel.py
+++ docs/code/gdal/batch_parallel.py
@@ -1,29 +1,21 @@
 import os
 from multiprocessing import Pool
-from timeit import default_timer as timer
 
 input_dir = 'naip'
+command_string = 'gdal_translate -of GTiff -co COMPRESS=JPEG {input} {output}'
 
-command = 'gdal_translate -of GTiff -co COMPRESS=JPEG {input} {output}'
+num_cores = 4
 
 def process(file):
     input = os.path.join(input_dir, file)
     filename = os.path.splitext(os.path.basename(file))[0]
     output =  os.path.join(input_dir, filename + '.tif')
-    os.system(command.format(input=input, output=output))
+    command = command_string.format(input=input, output=output)
+    print('Running ->', command)
+    os.system(command)
     
 files = [file for file in os.listdir(input_dir) if file.endswith('.jp2')]
 
 if __name__ == '__main__':
-  start = timer()
-  p = Pool(4)
+  p = Pool(num_cores)
   p.map(process, files)
-  end = timer()
-  print(end - start)
-  
-  start = timer()
-  for file in files:
-    process(file)
-  end = timer()
-  print(end - start)
-
