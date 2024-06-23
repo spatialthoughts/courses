ea10e0f118899c4dc9764555aa33502063f22682
Ujaval Gandhi <ujaval@spatialthoughts.com>
2024-06-23 14:48:19 +0530

    delete

--- code/python_dataviz/solutions/05_xarray_basics.py
+++
@@ -1,5 +0,0 @@
-selected = da.sel(lat=28.6, lon=77.2, method='nearest')
-max_anomaly = selected.where(selected==selected.max(), drop=True)
-
-print(max_anomaly.time.values[0])
-print(max_anomaly.time.dt.strftime('%Y-%m-%d').values[0])
