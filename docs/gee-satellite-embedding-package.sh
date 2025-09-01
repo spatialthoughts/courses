b5904c7903c4d46d5a1b8da055fbc7065f2f5c94
Ujaval Gandhi <ujaval@spatialthoughts.com>
2025-09-01 23:21:47 +0530

    update

---
+++ docs/gee-satellite-embedding-package.sh
@@ -0,0 +1,4 @@
+# rm -R code/end_to_end_gee
+# mkdir code/end_to_end_gee
+cp -R ~/projects/GEE-Satellite-Embedding/Hands_On code/gee_satellite_embedding/
+cp -R ~/projects/GEE-Satellite-Embedding/Supplement code/gee_satellite_embedding/
